1e089adbfe07d7a4ef689148b5a8af62e6296cae
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2024-02-15 07:55:21 -0400

    Modelado de una vista

---
+++ portalinternacional/dali-divint-webapp/src/main/java/com/indeval/portalinternacional/presentation/controller/divinternacional/ConsultaCustodiosController.java
@@ -0,0 +1,18 @@
+package com.indeval.portalinternacional.presentation.controller.divinternacional;
+
+import com.indeval.portalinternacional.presentation.controller.common.ControllerBase;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ConsultaCustodiosController  extends ControllerBase {
+
+    public ConsultaCustodiosController() {
+    }
+
+    public String getInicializar() {
+
+
+        return null;
+    }
+}
